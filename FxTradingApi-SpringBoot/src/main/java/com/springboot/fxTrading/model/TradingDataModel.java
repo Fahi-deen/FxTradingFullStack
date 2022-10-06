eb3266b72b4a8c9c268634e6fc16985a981526e1
Fahi-deen <fahideen786@gmail.com>
2022-10-06 15:41:00 +0530

    Added basic Unit test

--- FxTradingApi-SpringBoot/src/main/java/com/springboot/fxTrading/model/TradingDataModel.java
+++ FxTradingApi-SpringBoot/src/main/java/com/springboot/fxTrading/model/TradingDataModel.java
@@ -7,11 +7,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "tradedata")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TradingDataModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,4 +28,5 @@ public class TradingDataModel {
 	private double amount;
 	private float rate=66.0f;
 	private String status="processing";
+	
 }
--- FxTradingApi-SpringBoot/src/test/java/com/springboot/fxTrading/FxTradingApiApplicationTests.java
+++ FxTradingApi-SpringBoot/src/test/java/com/springboot/fxTrading/FxTradingApiApplicationTests.java
@@ -1,13 +1,49 @@
 package com.springboot.fxTrading;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.springboot.fxTrading.model.TradingDataModel;
+import com.springboot.fxTrading.repository.TradingDataRepository;
+import com.springboot.fxTrading.service.TradeService;
+
 @SpringBootTest
+//@TestMethodOrder(OrderAnnotation.class)
 class FxTradingApiApplicationTests {
 
+	@Autowired
+	TradingDataRepository testDataRepository;
+	@Autowired
+	TradeService testTradeService;
+
 	@Test
-	void contextLoads() {
+//	@Order(1)
+	public void testBookTrade() {
+		TradingDataModel book = TradingDataModel.builder().customerName("Test Fahi").currencyPair("USDINR").amount(1000).status("processing").build();
+		testTradeService.bookTrade(book);
+		testTradeService.printTrade();
+		System.out.println(testTradeService.printTrade());
+		assertNotNull(testDataRepository.findById(1L).get());
+	}
+
+	@Test
+	public void testprintTrades() {
+		List<TradingDataModel> allTrades = testTradeService.printTrade();
+		System.out.println(allTrades);
+		assertThat(allTrades).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testSingleTrade() {
+		TradingDataModel data = testDataRepository.findById(11L).get();
+		assertEquals("Test1", data.getCustomerName());
 	}
 
 }
